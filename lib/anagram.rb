2bb5db1c124b4185e48e332c5e2ad099e8034dc5
Allison Bohannon <akboh.github@gmail.com>
2022-10-09 15:16:12 -0700

    commit tree 36ce9ef5cbaf4e0a6a0549520b9e15950d2e46bc

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,32 @@
 # Your code goes here!
+require "pry"
+
+class Anagram
+
+    attr_reader :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(arr)
+        word_arr = word.chars.sort
+        match_words = []
+       
+        
+        arr.each do |check_word|
+            check_word_arr = check_word.chars.sort
+            if word_arr == check_word_arr
+                match_words.push(check_word)
+            end
+        end
+
+        puts match_words
+        match_words
+
+    end
+
+
+end
+
+binding.pry
